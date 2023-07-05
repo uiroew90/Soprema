3286165f3fd31273c7b7a2a4a9b46c6dd6d42569
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-05 13:27:56 +0100

    Create packs.json and courses.json files to store data for project

--- projects/courses/src/components/pages/Courses.js
+++ projects/courses/src/components/pages/Courses.js
@@ -1,16 +1,29 @@
 import { h, Fragment } from 'preact';
 import { Link } from 'preact-router';
+import { useEffect, useState } from 'preact/hooks';
 
 import Breadcrumb from '../sections/Breadcrumb';
 import Title from '../sections/Title';
 import Callout from '../sections/Callout';
 
-export default () => (
-  <Fragment>
-    <Breadcrumb links={[{ href: '/', text: 'Home' }, { href: '/packs', text: 'Firmenkurse' }, { text: 'Kursliste' }]} />
+export default () => {
+  const [courses, setCourses] = useState([]);
 
-    <Title text="Kursliste" />
+  useEffect(() => {
+    fetch('https://cdn.jsdelivr.net/gh/Demodia/Soprema/projects/courses/data/courses.json')
+      .then(response => response.json())
+      .then(data => setCourses(data));
+  }, []);
 
-    <Callout />
-  </Fragment>
-);
+  return (
+    <Fragment>
+      <Breadcrumb links={[{ href: '/', text: 'Home' }, { href: '/packs', text: 'Firmenkurse' }, { text: 'Kursliste' }]} />
+
+      <Title text="Kursliste" />
+
+      <Callout />
+
+      {/* Render courses here using the `courses` state variable */}
+    </Fragment>
+  );
+};
--- projects/courses/src/components/pages/Packs.js
+++ projects/courses/src/components/pages/Packs.js
@@ -1,16 +1,29 @@
 import { h, Fragment } from 'preact';
 import { Link } from 'preact-router';
+import { useEffect, useState } from 'preact/hooks';
 
 import Breadcrumb from '../sections/Breadcrumb';
 import Title from '../sections/Title';
 import Callout from '../sections/Callout';
 
-export default () => (
-  <Fragment>
-    <Breadcrumb links={[{ href: '/', text: 'Home' }, { text: 'Firmenkurse' }]} />
+export default () => {
+  const [packs, setPacks] = useState([]);
 
-    <Title text="Firmenkurse" />
+  useEffect(() => {
+    fetch('https://cdn.jsdelivr.net/gh/Demodia/Soprema/projects/courses/data/packs.json')
+      .then(response => response.json())
+      .then(data => setPacks(data));
+  }, []);
 
-    <Callout />
-  </Fragment>
-);
+  return (
+    <Fragment>
+      <Breadcrumb links={[{ href: '/', text: 'Home' }, { text: 'Firmenkurse' }]} />
+
+      <Title text="Firmenkurse" />
+
+      <Callout />
+
+      {/* Render packs here using the `packs` state variable */}
+    </Fragment>
+  );
+};
