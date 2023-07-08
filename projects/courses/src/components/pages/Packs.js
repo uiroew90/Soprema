e8cf6a906f394377d2e1b007fa8b654383191062
Kyle Schuller <33623211+KyleSchuller@users.noreply.github.com>
2023-07-08 13:21:25 +0100

    Update Packs

--- projects/courses/src/components/pages/Packs.js
+++ projects/courses/src/components/pages/Packs.js
@@ -5,7 +5,33 @@ import Breadcrumb from '../sections/Breadcrumb';
 import Title from '../sections/Title';
 import Callout from '../sections/Callout';
 
-export default () => (
+const Pack = ({ pack, setSelectedCourses }) => {
+  const handleClick = () => {
+    setSelectedCourses(pack.courses);
+  };
+
+  return (
+    <div class="guides-element custom-guides-element span-4">
+      <div class="small-tule small-tule--light small-tule--image-link">
+        <Link href="/courses" class="small-tule-wrapper" onClick={handleClick}>
+          <div class="small-tule-container">
+            <div class="small-tule-content wysiwyg">
+              <h3 class="small-tule-title">{pack.name}</h3>
+              <ul>
+                {pack.description.map(desc => (
+                  <li>{desc}</li>
+                ))}
+              </ul>
+              <p class="small-tule-button small-tule--custom-button">&nbsp;</p>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+const Packs = ({ packs, setSelectedCourses }) => (
   <Fragment>
     <Breadcrumb links={[{ href: '/', text: 'Home' }, { text: 'Firmenkurse' }]} />
 
@@ -19,56 +45,9 @@ export default () => (
           <div class="col col-sm-12">
             <div class="guides noresize" style="">
               <div class="guides-wrapper custom-guides-wrapper">
-                <div class="guides-element custom-guides-element span-4">
-                  <div class="small-tule small-tule--light small-tule--image-link">
-                    <Link href="/courses" class="small-tule-wrapper">
-                      <div class="small-tule-container">
-                        <div class="small-tule-content wysiwyg">
-                          <h3 class="small-tule-title">Flüssigkunststoffe</h3>
-                          <ul>
-                            <li>Nach Abschluss des Kurses ist jeder Teilnehmer in der Lage, den Untergrund zu beurteilen und die richtige Grundierung einzusetzen.</li>
-                            <li>Nach Abschluss des Kurses ist jeder Teilnehmer in der Lage, Detailabdichtungen mit Flüssigkunststoff auszuführen.</li>
-                            <li>Nach Abschluss des Kurses ist jeder Teilnehmer in der Lage, Detailabdichtungen und kleinere Flächenbeschichtungen mit Alsan PMMA selbstständig auszuführen.</li>
-                            <li>Nach Abschluss des Kurses ist jeder Teilnehmer in der Lage, verschiedene dekorative Oberflächen mit Alsan PMMA Produkten zu gestalten.</li>
-                          </ul>
-                          <p class="small-tule-button small-tule--custom-button">&nbsp;</p>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-                <div class="guides-element custom-guides-element span-4">
-                  <div class="small-tule small-tule--light small-tule--image-link">
-                    <Link href="/courses" class="small-tule-wrapper">
-                      <div class="small-tule-container">
-                        <div class="small-tule-content wysiwyg">
-                          <h3 class="small-tule-title">Bituminös</h3>
-                          <ul>
-                            <li>Jede/r Teilnehmer/in lernt die Verlegerichtlinien der Bitumen Abdichtungen kennen und die wichtigsten Verarbeitungspunkte und Details selbstständig auszuführen.</li>
-                            <li>Jede/r Teilnehmer/in lernt die Verlegerichtinien der verschiedenen Bewegungsfugenbänder kennen und die wichtigsten Verarbeitungspunkte und Details selbstständig auszuführen.</li>
-                          </ul>
-                          <p class="small-tule-button small-tule--custom-button">&nbsp;</p>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
-                <div class="guides-element custom-guides-element span-4">
-                  <div class="small-tule small-tule--light small-tule--image-link">
-                    <Link href="/courses" class="small-tule-wrapper">
-                      <div class="small-tule-container">
-                        <div class="small-tule-content wysiwyg">
-                          <h3 class="small-tule-title">Höhensicherungssysteme</h3>
-                          <ul>
-                            <li>Vorschriftsgemässe und korrekte Montage von Grün und Soprasafe Höhensicherungen. Nach Abschluss der Schulung wird ein Zertifikat ausgestellt, welches den Teilnehmer berechtigt, die Grün- und Soprasafe-Systeme zu montieren.</li>
-                            <li>Vorschriftsgemässe Planung von Höhensicherungen.</li>
-                          </ul>
-                          <p class="small-tule-button small-tule--custom-button">&nbsp;</p>
-                        </div>
-                      </div>
-                    </Link>
-                  </div>
-                </div>
+                {packs.map(pack => (
+                  <Pack pack={pack} setSelectedCourses={setSelectedCourses} />
+                ))}
               </div>
             </div>
           </div>
@@ -77,3 +56,5 @@ export default () => (
     </div>
   </Fragment>
 );
+
+export default Packs;
